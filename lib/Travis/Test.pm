365833b404858120fd9b63c0d75b2c858009ef17
brian greenfield <briang@cpan.org>
2018-09-06 14:44:17 +0100

    ddd

--- lib/Travis/Test.pm
+++ lib/Travis/Test.pm
@@ -10,25 +10,7 @@ Travis::Test - A Test. For Travis. Duh.
 
 =head1 VERSION
 
-This document describes Travis::Test version 1
-
-=head1 BUILD STATUS
-
-=for html
-<table>
-  <tr>
-    <td>Travis CI:</td>
-    <td><img
-      src="https://travis-ci.org/briang/travis-test.svg?branch=master"
-      alt="TravisCI Status"/></td>
-  </tr>
-  <tr>
-    <td>AppVeyor:</td>
-    <td><img
-      src="https://ci.appveyor.com/api/projects/status/github/briang/travis-test?branch=master&svg=true"
-      alt="AppVeyor Status"/></td>
-  </tr>
-</table>
+This document describes Travis::Test version 1.0
 
 =cut
 
